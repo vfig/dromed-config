abf39f80f043f8ab198b09457a27ddd23e6b2be5
Andy Durdin <me@andy.durdin.net>
2021-08-22 23:16:35 +0100

    press ? to get grid+nudge scale to match again (at 13)

---
+++ Common/Cmds/GridReset.cmd
@@ -0,0 +1,17 @@
+; reset nudge scale (min 1, max 1024) to default of 4
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 0
+global_scale 1
+global_scale 1
+; zoom back in to compensate (default camera scale is 64, but cant change this?)
+;zoom_all 0.5
+; reset to grid scale 13
+grid_abs_scale 0.5
