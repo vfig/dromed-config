d426e7f7dd805daacd90b343f1c0bf66db45e8f5
Andy Durdin <me@andy.durdin.net>
2021-08-27 10:31:08 +0100

    grid reset now goes to scale 14

--- Common/Cmds/GridReset.cmd
+++ Common/Cmds/GridReset.cmd
@@ -1,3 +1,5 @@
+;; Reset both grids to 13 (0.5 unit scale)
+
 ; reset nudge scale (min 1, max 1024) to default of 4
 global_scale 0
 global_scale 0
@@ -15,3 +17,12 @@ global_scale 1
 ;zoom_all 0.5
 ; reset to grid scale 13
 grid_abs_scale 0.5
+
+;; And increase to 14 (1 unit scale)
+
+; zoom out / increase nudge scale
+global_scale 1
+; zoom back in to compensate
+zoom_all 0.5
+; increase grid scale
+grid_scale 2.0
