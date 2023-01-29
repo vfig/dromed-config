c7ec08a09c4a1e1f3b690c2ddbab2e663b6d31cf
Andy Durdin <me@andy.durdin.net>
2023-01-29 06:40:14 +0000

    pgup/pgdn, shift+pgup/pgdn to move camera/brush by 512 units on Z (for Dual)

---
+++ Common/Cmds/CamUp512.cmd
@@ -0,0 +1,5 @@
+; NOTE: with speed_fac 1.0, camera movement is 1.5 times the nudge size! so,
+;       assuming the nudge scale is 14, we set speed to 512/1.5:
+speed_fac 341.3333333333333
+cam_slew 2
+speed_fac 1.0
---
+++ Common/Cmds/NudgeUp512.cmd
@@ -0,0 +1,7 @@
+; Assuming the grid/nudge scale is 14 (one unit):
+; NOTE: could use grid_abs_scale, but would desync the grid+nudge scales.
+set translate_by_grid
+grid_scale 512.0
+brush_translate 2
+grid_scale 0.001953125
+unset translate_by_grid
